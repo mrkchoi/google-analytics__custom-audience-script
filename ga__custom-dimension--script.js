9f308f7fc9ba5eab01056c8795fbbf80e3f5d332
mrkchoi <40152546+mrkchoi@users.noreply.github.com>
2018-10-17 15:29:17 -0700

    Updated to working script
    Be sure to read through the comments for implementation caveats.

--- ga__custom-dimension--script.js
+++ ga__custom-dimension--script.js
@@ -1,19 +1,24 @@
-
-
-
-
 // Working Script. Google Analytics Custom Dimension script implemented through Google Tag Manager as a custom variable. In this case, the script looks through the page html to ensure that the page is an article page, and then loops through the atricle categories to see if there is a text content match with our desired category filter. 
-function(){
+
+
+// WORKING SCRIPT
+// Ensure that the trigger that the script is attached to is set to fire after the window is completed loaded.
+
+function() {
     var category = document.querySelectorAll('.entry-category .gridlove-cat'); 
     var article = document.querySelectorAll('meta');
 
     for(var i = 0; i < article.length; i++) {
         if(article[i].getAttribute('property') == 'og:type' && article[i].getAttribute('content') == 'article') {
-            for(var i = 0; i < category.length; i++) {
-                if(category[i].textContent == 'Technology, Media and Telecommunications (TMT)') {
-                    return category[i].textContent;
-                } 
-            }
-        }    
+
+                for(var x = 0; x < category.length; x++) {
+                    if(category[x].textContent === 'Technology, Media and Telecommunications (TMT)') {
+                        return category[x].textContent;
+                    }
+                }
+            
+        } else {
+            continue;
+        }
     }
 }
